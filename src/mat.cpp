3f834464dfd11797c25b0e440f98e19238e786c4
gittopper <fedorovsnv@gmail.com>
2014-07-08 02:32:05 +0400

    move files

---
+++ src/mat.cpp
@@ -0,0 +1,78 @@
+#include "mat.h"
+
+namespace Geometry
+{
+	Mat::Mat(IntVector v1,IntVector v2, IntVector v3)
+	{
+		el(0,0) = v1[0];
+		el(1,0) = v1[1];
+		el(2,0) = v1[2];
+
+		el(0,1) = v2[0];
+		el(1,1) = v2[1];
+		el(2,1) = v2[2];
+
+		el(0,2) = v3[0];
+		el(1,2) = v3[1];
+		el(2,2) = v3[2];
+	}
+	double Mat::det()const
+	{
+		return el(0,0)*el(1,1)*el(2,2)+el(1,0)*el(2,1)*el(0,2)+el(0,1)*el(1,2)*el(2,0) - el(2,0)*el(1,1)*el(0,2)-el(1,0)*el(0,1)*el(2,2)-el(2,1)*el(1,2)*el(0,0);
+	}
+	void Mat::scale(double val)
+	{
+		for (int i =0; i <9; i++) 
+		{
+			double r = ar[i] * val;
+			ar[i] = r;
+			//BREAK_ON_LINE(ar[i] == r);
+		}
+	}
+	Mat Mat::inverse()
+	{
+		Mat inv;
+		inv.el(0,0) = (el(1,1) * el(2,2) - el(1,2) * el(2,1));
+		inv.el(0,1) = (el(0,2) * el(2,1) - el(0,1) * el(2,2));
+		inv.el(0,2) = (el(0,1) * el(1,2) - el(0,2) * el(1,1));
+
+		inv.el(1,0) = (el(1,2) * el(2,0) - el(1,0) * el(2,2));
+		inv.el(1,1) = (el(0,0) * el(2,2) - el(0,2) * el(2,0));
+		inv.el(1,2) = (el(0,2) * el(1,0) - el(0,0) * el(1,2));
+
+		inv.el(2,0) = (el(1,0) * el(2,1) - el(1,1) * el(2,0));
+		inv.el(2,1) = (el(0,1) * el(2,0) - el(0,0) * el(2,1));
+		inv.el(2,2) = (el(0,0) * el(1,1) - el(0,1) * el(1,0));
+		inv.scale(1./det());
+		return inv;
+	}
+	bool Mat::operator == (const Mat& m) const
+	{
+		for (int i=0;i<9;i++)
+		{
+			if (ar[i] != m.ar[i]) return false;
+		}
+		return true;
+	}
+	Mat Mat::operator * (const Mat& m) const
+	{
+		Mat n;
+		for (int i=0;i<3;i++)
+		{
+			for (int j=0;j<3;j++)
+			{
+				n.el(i,j) = el(i,0) * m.el(0,j) + el(i,1) * m.el(1,j) + el(i,2) * m.el(2,j);
+			}
+		}
+		return n;
+	}
+
+	IntVector Mat::operator * (const IntVector& v) const
+	{
+		IntVector o;
+		o[0] = int(el(0,0) * v[0] + el(0,1) * v[1] + el(0,2) * v[2]);
+		o[1] = int(el(1,0) * v[0] + el(1,1) * v[1] + el(1,2) * v[2]);
+		o[2] = int(el(2,0) * v[0] + el(2,1) * v[1] + el(2,2) * v[2]);
+		return o;
+	}
+}
---
+++ src/volpart.cpp
@@ -0,0 +1,126 @@
+#include "volpart.h"
+
+namespace Geometry
+{
+		bool VolPart::operator==(const VolPart& vol) const
+		{
+			return xyz == vol.xyz && fillInfo == vol.fillInfo && ( fillInfo != Angle || dir == vol.dir); 
+		}
+
+		bool VolPart::hasSide(int dim, int val) const
+		{
+			if (fillInfo == Empty) return false;
+			if (fillInfo == Full) return true;
+			BREAK_ON_LINE(!dir[dim] || dir[dim] * dir[dim] == 1);
+			return dir[dim] * (1 - 2 * val) >= 0;
+		}
+
+		VolPart& VolPart::operator+=(const VolPart& v)
+		{
+			BREAK_ON_LINE(xyz == v.xyz);
+			if (fillInfo == Empty)
+			{
+				fillInfo = v.fillInfo;
+				dir = v.dir;
+				return *this;
+			}
+			if (fillInfo == Angle && v.fillInfo == Angle && dir == -v.dir)
+			{
+				fillInfo = Full;
+				return *this;
+			}
+			BREAK_ON_LINE(false);
+			return *this;
+		}
+
+		VolPart& VolPart::rotate(RotType rot)
+		{
+			BREAK_ON_LINE(fillInfo != Empty);
+			xyz.rotate(rot);
+			if (fillInfo == Angle) dir.rotate(rot);
+			return *this;
+		}
+
+		VolPart& VolPart::rotate(const Mat& m)
+		{
+			xyz = m * xyz;
+			if (fillInfo == Angle)  dir = m * dir;
+			return *this;
+		}
+
+		VolPart& VolPart::operator-=(const VolPart& another)
+		{
+			if (fillInfo == Full)
+			{
+				if (another.fillInfo == Full)
+				{
+					fillInfo = Empty;
+					return *this;
+				}
+				else if (another.fillInfo == Angle)
+				{
+					fillInfo = Angle;
+					dir = -another.dir;
+					return *this;
+				}
+				else
+				{
+					BREAK_ON_LINE(false);
+				}
+			}
+			if (fillInfo == Angle)
+			{
+				if (another.fillInfo == Angle && dir == another.dir)
+				{
+					fillInfo = Empty;
+					return *this;
+				}
+				else
+				{
+					BREAK_ON_LINE(false);
+				}
+			}
+			return *this;
+		}
+		bool VolPart::couldPlace(const VolPart& another) const
+		{
+			BREAK_ON_LINE(xyz == another.xyz && another.fillInfo != Empty);
+			return fillInfo == Empty
+				|| fillInfo == Angle && another.fillInfo == Angle && dir == -another.dir
+				|| fillInfo == Full && another.fillInfo == Empty; 
+		}
+		bool VolPart::match(const VolPart& another) const
+		{
+			return fillInfo != Empty && another.fillInfo != Empty && !wall && !another.wall;
+		}
+		bool VolPart::shareOneOfSides(const VolPart& another) const
+		{
+			BREAK_ON_LINE(dot(xyz - another.xyz, xyz - another.xyz)<=1);
+
+			if (fillInfo == Empty || another.fillInfo == Empty || wall || another.wall) return false;
+			if (fillInfo == Full && another.fillInfo == Full) return true;
+			if (fillInfo == Angle && another.fillInfo == Full)
+			{
+				return dot(dir, another.xyz - xyz) < 0;
+			}
+			if (fillInfo == Full && another.fillInfo == Angle)
+			{
+				return dot(another.dir, xyz - another.xyz) < 0;
+			}
+			if (fillInfo == Angle && another.fillInfo == Angle)
+			{
+				//return true;
+				if (xyz == another.xyz)
+				{
+					if (dir == -another.dir) return true;
+				}
+				else
+				{
+					return dot(dir, another.xyz - xyz) < 0 && dot(another.dir, xyz - another.xyz) < 0;
+				}
+			}
+			cout << "problem 3\n";
+			BREAK_ON_LINE(false);
+			return false;
+		}
+}
