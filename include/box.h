d229417f5e4c27ba0ae545f5a6ddd379a3e83670
gittopper <fedorovsnv@gmail.com>
2014-07-08 01:47:24 +0400

    creating folder include

---
+++ include/box.h
@@ -0,0 +1,21 @@
+#include "base.h"
+#include "geometry.h"
+
+
+//test
+namespace Geometry
+{
+	class Box
+	{
+	public:
+		Box(int DX, int DY, int DZ):
+		  dimX(DX),dimY(DY),dimZ(DZ){}
+		VolPart& el(int x,y,z)
+		{
+			return box[x * dimZ * dimY + y * dimZ + z];
+		}
+	private:
+		vector<VolPart> box;
+		int dimX, dimY, dimZ;
+	};
+}
---
+++ include/utils.h
@@ -0,0 +1,10 @@
+#include "geometry.h"
+
+void generatePuzzles(vector<PuzzlePart>& puzzles);
+
+void generateSomaPuzzles(vector<PuzzlePart>& puzzles);
+
+void generatePuzzles2(vector<PuzzlePart>& puzzles);
+
+void initBox(Box& volume,int dimX,int dimY,int dimZ);
+
