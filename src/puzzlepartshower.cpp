eb278bd6ab08b36fefe9cf41878fc8fd44ca8039
gittopper <fedorovsnv@gmail.com>
2014-07-22 02:37:35 +0400

    square draw update

--- src/puzzlepartshower.cpp
+++ src/puzzlepartshower.cpp
@@ -7,12 +7,15 @@ using namespace Visualization;
 using namespace Geometry;
 
 namespace {
-	void drawSquare(const IntVector& v1,const IntVector& v2,const IntVector& v3,const IntVector& v4)
+	void drawSquare(const IntVector& v1,const IntVector& v2,const IntVector& v3,const IntVector& v4,const IntVector& n)
 	{
+		glBegin(GL_POLYGON);
 		glVertex3f(  v1[0], v1[1], v1[2]);
 		glVertex3f(  v2[0], v2[1], v2[2]);
 		glVertex3f(  v3[0], v3[1], v3[2]);
 		glVertex3f(  v4[0], v4[1], v4[2]);
+		glNormal3f(  n[0], n[1], n[2]);
+		glEnd();
 	}
 	void drawTriangle(const IntVector& v1,const IntVector& v2,const IntVector& v3,const IntVector& n)
 	{
@@ -40,47 +43,29 @@ void VolPartDrawer::draw(const VolPart& v) const
 	////////////////////////////////
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(1,0,0))>0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords(),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0));
-		glNormal3f(-1.0, 0.0, 0.0);
-		glEnd();
+		drawSquare(v.getCoords(),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0),IntVector(-1.0, 0.0, 0.0));
 	}
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(1,0,0))<0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,1));
-		glNormal3f(1.0, 0.0, 0.0);
-		glEnd();
+		drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,1),IntVector(1.0, 0.0, 0.0));
 	}
 	////////////////////////////////
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(0,1,0))>0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords(),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1));
-		glNormal3f(0.0, -1.0, 0.0);
-		glEnd();
+		drawSquare(v.getCoords(),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1),IntVector(0.0, -1.0, 0.0));
 	}
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(0,1,0))<0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,1,0));
-		glNormal3f(0.0, 1.0, 0.0);
-		glEnd();
+		drawSquare(v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,1,0),IntVector(0.0, 1.0, 0.0));
 	}
 	////////////////////////////////
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(0,0,1))>0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords(),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,0));
-		glNormal3f(0.0, 0.0, -1.0);
-		glEnd();
+		drawSquare(v.getCoords(),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,0),IntVector(0.0, 0.0, -1.0));
 	}
 	if (v.type() == VolPart::Full || dot(v.getDir(),IntVector(0,0,1))<0)
 	{
-		glBegin(GL_POLYGON);
-		drawSquare(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,1));
-		glNormal3f(0.0, 0.0, 1.0);
-		glEnd();
+		drawSquare(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,1),IntVector(0.0, 0.0, 1.0));
 	}
 	if (v.type() == VolPart::Angle)
 	{
@@ -89,111 +74,75 @@ void VolPartDrawer::draw(const VolPart& v) const
 		const IntVector& dir = v.getDir();
 		if (dot(dir,IntVector(1,1,0)) == 2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,1));
-			glNormal3f(s2, s2, 0.0);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,1),IntVector(s2, s2, 0.0));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,0,-1));
 			drawTriangle(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,0,1));
 		}
 		if (dot(dir,IntVector(1,1,0)) == -2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0));
-			glNormal3f(-s2, -s2, 0.0);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0),IntVector(-s2, -s2, 0.0));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(0,0,-1));
 			drawTriangle(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,1));
 		}
 		if (dir[0] == 1 && dir[1] == -1) 
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,0,0));
-			glNormal3f(s2, -s2, 0.0);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,0,0),IntVector(s2, -s2, 0.0));
 			drawTriangle(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(0,0,-1));
 			drawTriangle(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,1));
 		}
 		if (dir[0] == -1 && dir[1] == 1)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,1,0));
-			glNormal3f(-s2, s2, 0.0);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,1,0),IntVector(-s2, s2, 0.0));
 			drawTriangle(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,0,-1));
 			drawTriangle(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,0,1));
 		}
 		////////////////////////////////
 		if (dot(dir,IntVector(1,0,1)) == 2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0));
-			glNormal3f(s2, 0.0, s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0),IntVector(s2, 0.0, s2));
 			drawTriangle(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,-1,0));
 			drawTriangle(v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,0));
 		}
 		if (dot(dir,IntVector(1,0,1)) == -2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,0));
-			glNormal3f(-s2, 0.0, -s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,0),IntVector(-s2, 0.0, -s2));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,-1,0));
 			drawTriangle(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,0));
 		}
 		if (dir[0] == 1 && dir[2] == -1) 
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,1));
-			glNormal3f(s2, 0.0, -s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,1),IntVector(s2, 0.0, -s2));
 			drawTriangle(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,-1,0));
 			drawTriangle(v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0));
 		}
 		if (dir[0] == -1 && dir[2] == 1)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,0,0));
-			glNormal3f(-s2, 0.0, s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,0,0),IntVector(-s2, 0.0, s2));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,-1,0));
 			drawTriangle(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,0));
 		}
 		////////////////////////////////
 		if (dot(dir,IntVector(0,1,1)) == 2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,0));
-			glNormal3f(0.0, s2, s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,0),IntVector(0.0, s2, s2));
 			drawTriangle(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(-1,0,0));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,0,0));
 		}
 		if (dot(dir,IntVector(0,1,1)) == -2)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,0));
-			glNormal3f(0.0, -s2, -s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,0),IntVector(0.0, -s2, -s2));
 			drawTriangle(v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(-1,0,0));
 			drawTriangle(v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,0));
 		}
 		if (dir[1] == 1 && dir[2] == -1) 
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,0,0));
-			glNormal3f(0.0, s2, -s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(0,0,0),IntVector(0.0, s2, -s2));
 			drawTriangle(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,0,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(-1,0,0));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,1),v.getCoords() + IntVector(1,0,0));
 		}
 		if (dir[1] == -1 && dir[2] == 1)
 		{
-			glBegin(GL_POLYGON);
-			drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,0));
-			glNormal3f(0.0, -s2, s2);
-			glEnd();
+			drawSquare(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,0,0),IntVector(0.0, -s2, s2));
 			drawTriangle(v.getCoords() + IntVector(0,0,0),v.getCoords() + IntVector(0,1,1),v.getCoords() + IntVector(0,1,0),v.getCoords() + IntVector(-1,0,0));
 			drawTriangle(v.getCoords() + IntVector(1,0,0),v.getCoords() + IntVector(1,1,0),v.getCoords() + IntVector(1,1,1),v.getCoords() + IntVector(1,0,0));
 		}
